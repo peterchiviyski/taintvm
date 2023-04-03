39ab6b613945eb12bb0a4063db5915323c0dae17
peterchiviyski <peter.chiviyski@hashicorp.com>
2023-04-03 13:15:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,13 +16,6 @@ resource "aws_instance" "tainttestvanevm" {
   ami                    = "ami-00c39f71452c08778"
   instance_type          = "t2.micro"
 
-  user_data = <<EOF
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y nginx
-sudo systemctl start nginx
-EOF
-
   tags = {
     Name = "tainttest2"
   }
